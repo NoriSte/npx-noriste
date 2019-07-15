ecfaf8136e49cd55804c5b1818fd19faa19ccba6
NoriSte <nori.ste.magni@gmail.com>
2019-07-15 08:30:49 +0200

    feat: 🎸 Add the source code

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# noriste
+# npx-noriste
---
+++ src/index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+const boxen = require("boxen");
+const { bold } = require("chalk").default;
+const chalk = require("chalk");
+
+const color = "#f36";
+const chain = chalk.hex(color);
+
+const lines = [
+  chain.bold("Stefano Magni (NoriSte)"),
+  "Passionate, positive-minded Front-end developer.",
+  "JavaScript and testing lover.",
+  "",
+  bold("      Twitter: ") + chain("https://twitter.com/NoriSte"),
+  bold("     LinkedIn: ") + chain("https://www.linkedin.com/in/noriste/"),
+  bold("StackOverflow: ") + chain("https://stackoverflow.com/users/700707/noriste"),
+  bold("   Slides.com: ") + chain("https://slides.com/noriste"),
+  bold("       Resume: ") + chain("https://www.visualcv.com/stefano-magni/"),
+  bold("          NPM: ") + chain("https://www.npmjs.com/settings/noriste/packages")
+];
+console.log(lines);
+const message = lines.join("\n");
+
+const box = boxen(message, {
+  borderColor: color,
+  borderStyle: "round",
+  margin: 1,
+  padding: 1
+});
+
+process.stdout.write(box + "\n");
